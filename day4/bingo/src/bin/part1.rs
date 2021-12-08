e099447beece598f63c1ab1bedcdd031e37f909d
Greg Kent <gregokent@gmail.com>
2021-12-07 23:24:08 -0500

    part1 works lol

--- day4/bingo/src/bin/part1.rs
+++ day4/bingo/src/bin/part1.rs
@@ -30,27 +30,57 @@ impl Square {
 
 #[derive(Debug, Copy, Clone)]
 struct BingoBoard {
-    board: [[Square; 5]; 5],
+    board: [(usize, bool); 25],
 }
 
 impl BingoBoard {
     pub fn new() -> BingoBoard {
         BingoBoard {
-            board: [[Square::new();5];5]
+            board: [(0, false); 25],
         }
     }
 
     pub fn from_chunks(chunks: &[usize]) -> BingoBoard {
 
         let board = [
-            [ Square::from(chunks[0]),Square::from(chunks[1]),Square::from(chunks[2]),Square::from(chunks[3]),Square::from(chunks[4])],
-            [ Square::from(chunks[5]),Square::from(chunks[6]),Square::from(chunks[7]),Square::from(chunks[8]),Square::from(chunks[9])],
-            [ Square::from(chunks[10]),Square::from(chunks[11]),Square::from(chunks[12]),Square::from(chunks[13]),Square::from(chunks[14])],
-            [ Square::from(chunks[15]),Square::from(chunks[16]),Square::from(chunks[17]),Square::from(chunks[18]),Square::from(chunks[19])],
-            [ Square::from(chunks[20]),Square::from(chunks[21]),Square::from(chunks[22]),Square::from(chunks[23]),Square::from(chunks[24])],
+             (chunks[0],false),(chunks[1],false),(chunks[2],false),(chunks[3],false),(chunks[4],false),
+             (chunks[5],false),(chunks[6],false),(chunks[7],false),(chunks[8],false),(chunks[9],false),
+             (chunks[10],false),(chunks[11],false),(chunks[12],false),(chunks[13],false),(chunks[14],false),
+             (chunks[15],false),(chunks[16],false),(chunks[17],false),(chunks[18],false),(chunks[19],false),
+             (chunks[20],false),(chunks[21],false),(chunks[22],false),(chunks[23],false),(chunks[24],false),
         ];
 
         BingoBoard { board }
+    }
+
+    pub fn check_draw(&mut self, draw: usize) {
+        if let Some(mut spot) = self.board.iter_mut().find(|x| x.0 == draw) {
+            spot.1 = true;
+        }
+    }
+
+    pub fn check_win(&self) -> bool {
+        for index in (0..self.board.len()).step_by(5) {
+            if  self.board[index].1 &&
+                self.board[index+1].1 &&
+                self.board[index+2].1 &&
+                self.board[index+3].1 &&
+                self.board[index+4].1 {
+                    return true
+                }
+        }
+
+        for index in (0..self.board.len()).take(5) {
+            if self.board[index].1 &&
+                self.board[index+5].1 &&
+                self.board[index+10].1 &&
+                self.board[index+15].1 &&
+                self.board[index+20].1 {
+                    return true;
+                }
+        }
+
+        false
     }
 }
 
@@ -74,7 +104,24 @@ fn main() -> Result<(), Box<dyn Error + 'static + Send + Sync>> {
         if chunk.len() != 25 { break; }
         boards.push(BingoBoard::from_chunks(chunk));
     }
-    println!("{:?}", boards);
+
+    'outer: for draw in draws.iter() {
+        for mut board in &mut boards {
+            board.check_draw(*draw);
+            if board.check_win() {
+                println!("board wins!");
+                println!("{:?}", &board.board[0..5]);
+                println!("{:?}", &board.board[5..10]);
+                println!("{:?}", &board.board[10..15]);
+                println!("{:?}", &board.board[15..20]);
+                println!("{:?}", &board.board[20..25]);
+                let sum = board.board.iter().filter(|(_, marked)| *marked == false).fold(0, |acc, (num, _)| acc + num);
+                println!("sum = {}", sum * draw);
+                break 'outer;
+            }
+        }
+    }
+    //println!("{:?}", boards);
     //let mut lines = std::io::BufReader::new(File::open("input.txt")?).lines();
 
     //let lines: Vec<String> = lines.map(|s| s.unwrap())
